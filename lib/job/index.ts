57b41cb6b56042b891405e1b8bcafa210b3140a0
Dream Huang <dream.huang@yijinin.com>
2024-01-15 14:13:25 +0800

    feat: add

--- lib/job/index.ts
+++ lib/job/index.ts
@@ -1,8 +1,8 @@
 import { getContainerItem } from "./getContainerItem";
 import { mintDft } from "./mintDft";
 
-mintDft('dragon', 20000, {
-    satsbyte: 60
+mintDft('sophon', 20000, {
+    satsbyte: 45
 })
 
 // getContainerItem('#atomlizard1', 10000)
